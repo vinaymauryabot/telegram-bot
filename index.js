1b9b80f31d868369745cd1fc8f8bfc4e427a40f8
vinaymauryabot <vinaymauryaaigpt@gmail.com>
2026-03-02 16:35:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,3 +19,15 @@ bot.on("message", (msg) => {
     bot.sendMessage(msg.chat.id, "Keyword not found ❌\nType /start");
   }
 });
+// Dummy server for Render
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is running 🚀");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
