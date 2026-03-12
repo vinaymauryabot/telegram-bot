d9629ba02c117741e3a9129d50c72fea0aabff08
vinaymauryabot <vinaymauryaaigpt@gmail.com>
2026-03-12 11:21:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const TelegramBot = require("node-telegram-bot-api")
 const express = require("express")
 
 const token = process.env.BOT_TOKEN
-const bot = new TelegramBot(token,{polling:true})
+const bot = new TelegramBot(TOKEN, { polling: { interval: 3000, autoStart: true } });
 
 const app = express()
 
